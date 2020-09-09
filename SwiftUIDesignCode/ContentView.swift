be7b5c38666443a30d0afbec50716460b71c0937
Sergey <Batutist@gmail.com>
2020-09-08 23:01:56 -0700

    Initial Commit

---
+++ SwiftUIDesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDesignCode
+//
+//  Created by Sergey Kovalev on 9/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDesignCode/SwiftUIDesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignCodeApp.swift
+//  SwiftUIDesignCode
+//
+//  Created by Sergey Kovalev on 9/8/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
